e5305a64e4bd981db1c975a8944b0989fd24646e
cploujoux <ch.ploujoux@gmail.com>
2025-01-24 19:58:47 -0800

    Add typescript

---
+++ agents/typescript/0-hello-world/agent.ts
@@ -0,0 +1,45 @@
+import { getDefaultThread, wrapAgent } from "@beamlit/sdk";
+import { HumanMessage } from "@langchain/core/messages";
+import { CompiledGraph } from "@langchain/langgraph";
+import { FastifyRequest } from "fastify";
+import { v4 as uuidv4 } from "uuid";
+
+type InputType = {
+  inputs: string | null;
+  input: string | null;
+};
+
+type AgentType = {
+  agent: CompiledGraph<any, any, any, any, any, any>;
+};
+
+const req = async (request: FastifyRequest, args: AgentType) => {
+  const { agent } = args;
+  const body = (await request.body) as InputType;
+  const thread_id = getDefaultThread(request) || uuidv4();
+  const input = body.inputs || body.input || "";
+  const responses: any[] = [];
+
+  const stream = await agent.stream(
+    { messages: [new HumanMessage(input)] },
+    { configurable: { thread_id } }
+  );
+
+  for await (const chunk of stream) {
+    responses.push(chunk);
+  }
+  const content = responses[responses.length - 1];
+  return content.agent.messages[content.agent.messages.length - 1].content;
+};
+
+export const agent = wrapAgent(req, {
+  agent: {
+    metadata: {
+      name: "{{.ProjectName}}",
+    },
+    spec: {
+      model: "{{.Model}}",
+      description: "{{.ProjectDescription}}",
+    },
+  },
+});
